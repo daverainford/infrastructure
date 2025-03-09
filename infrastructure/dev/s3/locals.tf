cdc504f951eb86da555fd88ec6b452cd6170d3aa
daverainford <rainford.d.k@gmail.com>
2025-03-09 13:41:51 -0700

    Organized locals.tf

--- infrastructure/dev/s3/locals.tf
+++ infrastructure/dev/s3/locals.tf
@@ -1,13 +1,36 @@
-# locals.tf
 locals {
+  #-------------------------------------------------
+  # Common Tags - Applied to all resources
+  #-------------------------------------------------
   common_tags = {
-    env     = "dev"
-    project = "mosaic"
+    environment = "dev"
+    project     = "mosaic"
   }
-  
+
+  #-------------------------------------------------
+  # Lifecycle Configuration Parameters
+  #-------------------------------------------------
   lifecycle_configs = {
+    # Short term data storage (30 days)
     short_term = {
-      expiration_days = 14
+      expiration_days = 30
+    }
+  }
+
+  #-------------------------------------------------
+  # Lifecycle Rules
+  #-------------------------------------------------
+  short_term_lifecycle_rule = {
+    id     = "short-term-data-lifecycle"
+    status = "Enabled"
+    
+    transition = {
+      days          = 0
+      storage_class = "STANDARD"
+    }
+    
+    expiration = {
+      days = local.lifecycle_configs.short_term.expiration_days
     }
   }
 }
