f43e2c7a03dca01b91f9d433e910c31fc76752ca
daverainford <rainford.d.k@gmail.com>
2025-03-10 10:33:15 -0700

    Added KMS config

---
+++ infrastructure/dev/s3/kms.tf
@@ -0,0 +1,19 @@
+# KMS Key for S3 bucket encryption
+resource "aws_kms_key" "s3_encryption" {
+  description             = "KMS key for S3 bucket encryption"
+  deletion_window_in_days = local.kms_key_settings.deletion_window_in_days
+  enable_key_rotation     = local.kms_key_settings.enable_key_rotation
+  key_usage               = local.kms_key_settings.key_usage
+  
+  tags = merge(
+    local.common_tags,
+    {
+      type = "encryption"
+    }
+  ) 
+}
+
+resource "aws_kms_alias" "s3_encryption" {
+  name          = "alias/s3-encryption-key"
+  target_key_id = aws_kms_key.s3_encryption.key_id
+}
